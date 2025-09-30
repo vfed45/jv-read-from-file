eb2bffa5beac87398d3fda4007e2208035b093f8
Viktoriia <fvee737@gmail.com>
2025-09-30 18:44:14 +0200

    created the project

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,29 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+            String content;
+            try {
+                content = Files.readString(Path.of(fileName)); //write your code here
+            } catch (IOException e) {
+                return new String[0];
+            }
+            String lowerCaseContent = content.toLowerCase();
+            String[] allWords = lowerCaseContent.split("[\\s\\p{Punct}]+");
+            List<String> filteredWords = new ArrayList<>();
+            for (String word : allWords) {
+                if (!word.isEmpty() && word.startsWith("w")) {
+                    filteredWords.add(word);
+                }
+            }
+            Collections.sort(filteredWords);
+            return filteredWords.toArray(new String[0]);
+        }
     }
-}
